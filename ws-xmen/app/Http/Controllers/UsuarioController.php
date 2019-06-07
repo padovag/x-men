9cc63f019fbffa079eb3b7b9eb2c38b6c61b1222
Giulia <gcesarpadovani@gmail.com>
2019-06-07 19:06:18 -0300

    Fix controller

--- ws-xmen/app/Http/Controllers/UsuarioController.php
+++ ws-xmen/app/Http/Controllers/UsuarioController.php
@@ -5,38 +5,14 @@ namespace App\Http\Controllers;
 use App\Usuario;
 use Illuminate\Http\Request;
 
-class UsuarioController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
+class UsuarioController extends Controller {
+    public function index() {
         $usuarios = Usuario::all();
         return response()->json(['usuarios' => $usuarios]);
 
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
+    public function store(Request $request) {
         $usuario = new Usuario();
         $usuario->nome = $request['nome'];
         $usuario->usuario = $request['usuario'];
