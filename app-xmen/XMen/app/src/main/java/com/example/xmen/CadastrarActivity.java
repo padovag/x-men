d9e115c6576d99987ece2eb3b5089414bc98c4d0
Giulia Padovani <giuliacpadovani@gmail.com>
2019-06-14 00:20:24 -0300

    Finaliza cadastro de novos mutantes (sem imagem)

--- app-xmen/XMen/app/src/main/java/com/example/xmen/CadastrarActivity.java
+++ app-xmen/XMen/app/src/main/java/com/example/xmen/CadastrarActivity.java
@@ -1,14 +1,35 @@
 package com.example.xmen;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
 
 public class CadastrarActivity extends AppCompatActivity {
 
@@ -16,6 +37,9 @@ public class CadastrarActivity extends AppCompatActivity {
     ImageView foto;
     Button botao;
     Uri imageUri;
+    EditText nomeCampo, habilidadeCampo;
+    private AlertDialog alerta;
+    public static String nomeMutante;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +48,79 @@ public class CadastrarActivity extends AppCompatActivity {
 
         foto = (ImageView) findViewById(R.id.foto);
         botao = (Button) findViewById(R.id.escolherImagem);
+        nomeCampo = (EditText) findViewById(R.id.nome);
+        habilidadeCampo = (EditText) findViewById(R.id.habilidade);
+    }
+
+    public void onClickCadastrar(View view) {
+        String nome = nomeCampo.getText().toString();
+        String habilidade = habilidadeCampo.getText().toString();
+
+        cadastrarMutante(nome, habilidade, "foto não pode ser cadastrada", 1);
+    }
+
+    private void cadastrarMutante(String nome, String habilidade, String fotoBase64, int usuario_id) {
+        String url = getUrl(nome, habilidade, fotoBase64, usuario_id);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.POST, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        JSONObject mutante = null;
+                        try {
+                            mutante = response.getJSONObject("mutante");
+                            CadastrarActivity.nomeMutante = mutante.get("nome").toString();
+
+                            criaAlertDialog();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                });
+
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+    }
+
+    private String getUrl(String nome, String habilidade, String fotoBase64, int usuario_id) {
+        return "http://10.0.2.2:8000/api/mutantes?nome="
+                + nome
+                + "&habilidade="
+                + habilidade
+                + "&foto="
+                + fotoBase64
+                + "&usuario_id="
+                + usuario_id;
+    }
+
+    public void criaAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Cadastrar Mutante");
+        builder.setMessage("O mutante foi cadastrado com sucesso");
+
+        builder.setPositiveButton("Ver mutante", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+                Intent it = new Intent(getBaseContext(), MostraMutanteActivity.class);
+                Bundle b = new Bundle();
+                b.putString("nome", CadastrarActivity.nomeMutante);
+                it.putExtras(b);
+                startActivity(it);
+            }
+        });
+
+        builder.setNegativeButton("Voltar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+                Intent it = new Intent(getBaseContext(), DashboardActivity.class);
+                startActivity(it);
+            }
+        });
+        alerta = builder.create();
+        alerta.show();
     }
 
     public void onClickEscolherImagem(View view) {
---
+++ app-xmen/XMen/app/src/main/java/com/example/xmen/ImageUtils.java
@@ -0,0 +1,40 @@
+package com.example.xmen;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
+
+public class ImageUtils {
+    public static Bitmap drawableToBitmap (Drawable drawable) {
+        Bitmap bitmap = null;
+
+        if (drawable instanceof BitmapDrawable) {
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
+            if(bitmapDrawable.getBitmap() != null) {
+                return bitmapDrawable.getBitmap();
+            }
+        }
+
+        if(drawable.getIntrinsicWidth() <= 0 || drawable.getIntrinsicHeight() <= 0) {
+            bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888); // Single color bitmap will be created of 1x1 pixel
+        } else {
+            bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        }
+
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+        return bitmap;
+    }
+
+    private String bitmapToBase64(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream .toByteArray();
+        return Base64.encodeToString(byteArray, Base64.DEFAULT);
+    }
+}
